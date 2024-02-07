e1c2d7d6fe55408a571c05eb194326d4948493e6
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-02-07 12:08:22 -0800

    created test for  Post value in apiRoutes file.

--- Develop/routes/apiRoutes.test.js
+++ Develop/routes/apiRoutes.test.js
@@ -3,14 +3,12 @@ const express = require('express');
 const apiRoutes = require('./apiRoutes');
 const fs = require('fs').promises;
 
-jest.mock('fs', () => {
-    return {
-        promises: {
-            readFile: jest.fn(),
-            writefile: jest.fn(),
-        },
-    };
-});
+jest.mock('fs', () => ({
+    promises: {
+      readFile: jest.fn(),
+      writeFile: jest.fn(),
+    },
+  }));
 
 describe('API Routes', ()=> {
     let app;
@@ -37,5 +35,31 @@ describe('API Routes', ()=> {
             const response = await request(app).get('/api/notes');
             expect(response.statusCode).toBe(500);
         });
+    });
+
+    describe('POST /notes', () => {
+        it('should create a note then retun it', async () => {
+            const newNotes = {title: 'Test POST', text: 'This is a test note for POST' };
+            const mockNotes = [];
+
+            fs.readFile.mockResolvedValue(JSON.stringify(mockNotes));
+            fs.writeFile.mockResolvedValue();
+            
+            const response = await request(app)
+            .post('/api/notes')
+            .send(newNotes);
+
+            expect(response.statusCode).toEqual(200);
+            expect(response.body).toHaveProperty('id');
+            expect(response.body.title).toBe(newNotes.title);
+            expect(response.body.text).toBe(newNotes.text);
+
+            expect(fs.writeFile).toHaveBeenCalledWith(
+                expect.any(String),
+                expect.stringContaining(newNotes.title),
+                'utf8'
+            );
+        });
+
     });
 });
