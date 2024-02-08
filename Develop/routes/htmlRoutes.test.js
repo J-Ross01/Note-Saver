e0b1cb8c963093a681242cc9c641d322ae7e4834
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-02-07 16:50:56 -0800

    edited the route calls to my htmlRoutes file and needed to add a call to my express package.

--- Develop/routes/htmlRoutes.test.js
+++ Develop/routes/htmlRoutes.test.js
@@ -2,32 +2,38 @@ const express = require('express');
 const path = require('path');
 
 jest.mock('express', () => {
-  const originalModule = jest.requireActual('express');
   return {
-    ...originalModule,
-    Router: () => ({
+    __esModule: true, 
+    default: jest.fn(() => ({ 
+      use: jest.fn(),
+      listen: jest.fn(),
+    })),
+    Router: () => ({ 
       get: jest.fn().mockImplementation((path, callback) => callback({}, { sendFile: jest.fn() })),
     }),
   };
 });
 
 describe('HTML Routes', () => {
+  let app;
   let router;
 
   beforeEach(() => {
-    jest.resetModules(); 
-    router = require('./htmlRoutes'); 
+    jest.resetModules();
+    app = express.default(); 
+    router = require('./htmlRoutes');
+    app.use(router); 
   });
 
   it('should route /notes to notes.html', () => {
     const mockSendFile = jest.fn();
-    router.get.mock.calls[0][1](null, { sendFile: mockSendFile });
+    router.get.mock.calls.find(call => call[0] === '/notes')[1](null, { sendFile: mockSendFile });
     expect(mockSendFile).toHaveBeenCalledWith(path.join(__dirname, '..', 'public', 'notes.html'));
   });
 
   it('should route all other paths to index.html', () => {
     const mockSendFile = jest.fn();
-    router.get.mock.calls[1][1](null, { sendFile: mockSendFile });
+    router.get.mock.calls.find(call => call[0] === '*')[1](null, { sendFile: mockSendFile });
     expect(mockSendFile).toHaveBeenCalledWith(path.join(__dirname, '..', 'public', 'index.html'));
   });
 });
